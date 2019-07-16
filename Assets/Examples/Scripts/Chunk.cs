8ce92dd969751129f1bf75a71d849824d9a41086
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2019-07-16 17:47:50 +0300

    Removed unnecessary code from Chunk

--- Assets/Examples/Scripts/Chunk.cs
+++ Assets/Examples/Scripts/Chunk.cs
@@ -6,12 +6,10 @@ namespace MarchingCubes.Examples
     {
         [HideInInspector] public bool isDirty;
         [HideInInspector] public DensityField densityField;
-        [HideInInspector] public int chunkSize;
         [HideInInspector] public Vector3Int position;
 
         private MeshFilter _meshFilter;
         private MeshCollider _meshCollider;
-        private DensityGenerator _densityGenerator;
 
         private World world;
 
@@ -36,18 +34,11 @@ namespace MarchingCubes.Examples
 
         public void Initialize(World world, int chunkSize, Vector3Int position)
         {
-            this.chunkSize = chunkSize;
             this.position = position;
             this.world = world;
 
-            _densityGenerator = world.densityGenerator;
-
-            int worldPosX = position.x;
-            int worldPosY = position.y;
-            int worldPosZ = position.z;
-
             densityField = new DensityField(chunkSize + 1, chunkSize + 1, chunkSize + 1);
-            densityField.Populate(_densityGenerator.CalculateDensity, position);
+            densityField.Populate(world.densityGenerator.CalculateDensity, position);
         }
 
         public void Generate()
