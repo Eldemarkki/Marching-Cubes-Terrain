473c27f9ed73683ea2050949cf254b84bf12fe49
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2019-12-23 16:17:35 +0200

    Suggests a better size for the selected heightmap

--- Assets/Examples/Editor/HeightmapDensityInspector.cs
+++ Assets/Examples/Editor/HeightmapDensityInspector.cs
@@ -12,9 +12,20 @@ namespace MarchingCubes.Examples
             if (property.isExpanded)
             {
                 EditorGUI.indentLevel++;
-                EditorGUILayout.ObjectField(property.FindPropertyRelative("heightmap"), typeof(Texture2D), new GUIContent("Heightmap"));
+
+                HeightmapTerrainSettings settings = (HeightmapTerrainSettings)fieldInfo.GetValue(property.serializedObject.targetObject);
+                settings.Heightmap = (Texture2D)EditorGUILayout.ObjectField("Heightmap", settings.Heightmap, typeof(Texture2D), false);
+
+                if (Mathf.ClosestPowerOfTwo(settings.Heightmap.width) != settings.Heightmap.width - 1)
+                    EditorGUILayout.HelpBox("The width of the heightmap should be some power of 2 plus 1. Suggested width: " + (Mathf.ClosestPowerOfTwo(settings.Heightmap.width) + 1), MessageType.Warning);
+                if (Mathf.ClosestPowerOfTwo(settings.Heightmap.height) != settings.Heightmap.height - 1)
+                    EditorGUILayout.HelpBox("The height of the heightmap should be some power of 2 plus 1. Suggested height: " + (Mathf.ClosestPowerOfTwo(settings.Heightmap.height) + 1), MessageType.Warning);
+
+                fieldInfo.SetValue(property.serializedObject.targetObject, settings);
+
                 EditorGUILayout.PropertyField(property.FindPropertyRelative("amplitude"), new GUIContent("Amplitude"));
                 EditorGUILayout.PropertyField(property.FindPropertyRelative("heightOffset"), new GUIContent("Height Offset"));
+
                 EditorGUI.indentLevel--;
             }
         }
