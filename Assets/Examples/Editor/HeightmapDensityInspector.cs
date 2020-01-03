dc8d6b7747ce71f42e7fc183f8fd12f712222e1e
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2020-01-03 13:53:58 +0200

    Removed warning from heightmap inspector

--- Assets/Examples/Editor/HeightmapDensityInspector.cs
+++ Assets/Examples/Editor/HeightmapDensityInspector.cs
@@ -13,16 +13,7 @@ namespace MarchingCubes.Examples
             {
                 EditorGUI.indentLevel++;
 
-                HeightmapTerrainSettings settings = (HeightmapTerrainSettings)fieldInfo.GetValue(property.serializedObject.targetObject);
-                settings.Heightmap = (Texture2D)EditorGUILayout.ObjectField("Heightmap", settings.Heightmap, typeof(Texture2D), false);
-
-                if (Mathf.ClosestPowerOfTwo(settings.Heightmap.width) != settings.Heightmap.width - 1)
-                    EditorGUILayout.HelpBox("The width of the heightmap should be some power of 2 plus 1. Suggested width: " + (Mathf.ClosestPowerOfTwo(settings.Heightmap.width) + 1), MessageType.Warning);
-                if (Mathf.ClosestPowerOfTwo(settings.Heightmap.height) != settings.Heightmap.height - 1)
-                    EditorGUILayout.HelpBox("The height of the heightmap should be some power of 2 plus 1. Suggested height: " + (Mathf.ClosestPowerOfTwo(settings.Heightmap.height) + 1), MessageType.Warning);
-
-                fieldInfo.SetValue(property.serializedObject.targetObject, settings);
-
+                EditorGUILayout.ObjectField(property.FindPropertyRelative("heightmap"), typeof(Texture2D), new GUIContent("Heightmap"));
                 EditorGUILayout.PropertyField(property.FindPropertyRelative("amplitude"), new GUIContent("Amplitude"));
                 EditorGUILayout.PropertyField(property.FindPropertyRelative("heightOffset"), new GUIContent("Height Offset"));
 
