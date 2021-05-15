f9edbbdb2d70bac4c013ef5a852adddef9c0c39d
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2021-05-15 19:26:54 +0300

    Combine terrain painting and triplanar shader to allow both to work simultaneously

--- Assets/VoxelTerrain/Core/VoxelData/VoxelColorStore.cs
+++ Assets/VoxelTerrain/Core/VoxelData/VoxelColorStore.cs
@@ -12,24 +12,16 @@ namespace Eldemarkki.VoxelTerrain.VoxelData
     public class VoxelColorStore : PerVoxelStore<Color32>
     {
         /// <summary>
-        /// The default color that the terrain will be colored with on load
-        /// </summary>
-        [SerializeField] private Color32 defaultTerrainColor = new Color32(11, 91, 33, 255);
-
-        /// <summary>
         /// Generates the colors for a chunk at <paramref name="chunkCoordinate"/>, where the output array is <paramref name="outputColors"/> to save memory by not needing to allocate a new array. This does not check if a color array already exists at <paramref name="chunkCoordinate"/>
         /// </summary>
         /// <param name="chunkCoordinate">The coordinate of the chunk which to generate the colors for</param>
         /// <param name="outputColors">The array that should be filled with the new colors</param>
         public override unsafe JobHandle GenerateDataForChunkUnchecked(int3 chunkCoordinate, VoxelDataVolume<Color32> outputColors)
         {
-            // Fill the array with the default terrain color
-            Color32* defaultColorArray = stackalloc Color32[1] { defaultTerrainColor };
-
+            // Fill the array with the any color, with alpha=0 by default
+            Color32* defaultColorArray = stackalloc Color32[1] { new Color32(0, 0, 0, 0) };
             UnsafeUtility.MemCpyReplicate(outputColors.GetUnsafePtr(), defaultColorArray, sizeof(Color32), outputColors.Length);
-
             SetDataChunkUnchecked(chunkCoordinate, outputColors, false);
-
             return default;
         }
     }
